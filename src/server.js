c7f192221da6149100efc0b8ad2faf877b9dccc2
RosareMililani <mr7721@rit.edu>
2020-09-21 01:27:38 -0400

    finished demo

--- src/server.js
+++ src/server.js
@@ -7,7 +7,30 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const body = [];
 
+    // error occurs while trying to read information from client
+    // asynchronus
+    request.on('error', () => {
+      // console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
+
+    // get last chunk - new piece of info
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    // take all elements of array and combine
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      jsonHandler.addUser(request, response, bodyParams);
+    });
+  }
 };
 
 const handleGet = (request, response, parsedUrl) => {
@@ -23,9 +46,13 @@ const handleGet = (request, response, parsedUrl) => {
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
-
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
 };
 
 http.createServer(onRequest).listen(port);
 
-console.log(`Listening on 127.0.0.1: ${port}`);
+// console.log(`Listening on 127.0.0.1: ${port}`);
